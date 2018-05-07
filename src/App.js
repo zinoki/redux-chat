bf2c61e897458f35351e8cc035979fa88e456b57
Zachary <zinouye@gmail.com>
2018-05-06 17:50:57 -0700

    updated app to show useres, messages list, and new message section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="container">
+        <aside id="sidebar">Users</aside>
+          <section id="main">
+            <section id="messages-list">Messages List</section>
+            <section id="new-message">New Message</section>
+          </section>
+
       </div>
     );
   }
