dc8b8f7c9a2ba18617892e274ee9943d291afb24
Zachary <zinouye@gmail.com>
2018-05-07 19:30:34 -0700

    added add_message function

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,4 +4,11 @@ let nextMessageId = 0;
 const nextUserId = 0;
 
 export const addMessage = (message, author) => ({
-})
+  type: types.ADD_MESSAGE,
+  id: nextMessageId++,
+  message,
+  author
+});
+
+
+
