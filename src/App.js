39a097efd6a0efe545d5a7da288c2d0d4f80e8b1
Zachary <zinouye@gmail.com>
2018-05-07 18:59:52 -0700

    replaced html rendering with components that will be worked on

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Sidebar } from './containers/Sidebar';
+import { MessagesList } from './containers/MessagesList';
+import { AddMessage } from './containers/AddMessage';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div id="container">
-        <aside id="sidebar">Users</aside>
+        <Sidebar />
           <section id="main">
-            <section id="messages-list">Messages List</section>
-            <section id="new-message">New Message</section>
+            <MessagesList />
+            <AddMessage />
           </section>
 
       </div>
