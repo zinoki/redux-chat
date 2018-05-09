5cda72ff41e3004dfc46c71222c79edf0a9d06b1
Zachary <zinouye@gmail.com>
2018-05-09 14:38:56 -0700

    added MessagesList function to show all messages

---
+++ src/containers/MessageList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MessagesList = ({ messages }) => (
+  <section id="messages-list">
+    <ul>
+      {messages.map(message => (
+        <Messages
+          key={message.id}
+          {...message}
+          />
+      ))}
+    </ul>
+  </section>
+)
+
+MessagesList.PropTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export default MessagesList;
