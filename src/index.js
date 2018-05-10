009c68db7e469f74ece12adf8918d100a77decb8
Zachary <zinouye@gmail.com>
2018-05-10 16:03:19 -0700

    import setupSocket

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import chat from './reducers';
 import { addUser } from './actions';
+import { setupSocket } from './sockets';
 
 const store = createStore(chat);
 
